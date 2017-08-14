45ea73dc657a1e44a7b27538445daa7fc6cc8345
Gerd Reiss <gerdreiss@jscriptive.com>
2017-08-14 20:51:38 +0200

    Add module Simple

---
+++ Simple.hs
@@ -0,0 +1,14 @@
+module Simple
+where
+
+-- calculates the arithmetic mean of two numbers
+arithmetic_mean :: Fractional a => a -> a -> a
+arithmetic_mean x y = (x + y) / 2
+
+-- calculates the harmonic mean of two numbers
+harmonic_mean :: Fractional a => a -> a -> a
+harmonic_mean x y = 2 * x * y / (x + y)
+
+
+-- our own version of 'length'
+length' xs = sum [1 | _ <- xs]

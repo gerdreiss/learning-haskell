23a5678b39297d8f392e54257fc04fb2b8bff3b0
Gerd Reiss <gerdreiss@jscriptive.com>
2017-08-20 17:06:05 +0200

    Типы данных

--- ProcessData.hs
+++ ProcessData.hs
@@ -1,5 +1,6 @@
 module ProcessData where
 
+{- Variant 1 -}
 data Result = Fail | Success
 
 doSomeWork :: SomeData -> (Result, Int)
@@ -11,3 +12,16 @@ processData :: SomeData -> String
 processData d = case doSomeWork d of
     (_, 0) -> "Success"
     (_, x) -> "Fail: " ++ show x
+
+{- Variant 2 -}
+data Result' = Fail' Int | Success'
+
+instance Show Result' where
+    show Success'     = "Success"
+    show (Fail' code) = "Fail: " ++ show code
+
+doSomeWork' :: SomeData -> Result'
+doSomeWork' d = case dat of
+        (_,0)    ->  Success';
+        (_,code) -> (Fail' code)
+    where dat = doSomeWork d

f7508319e2de0a45e1b5acbc864cc9e05f6ea57d
Gerd Reiss <gerdreiss@jscriptive.com>
2017-10-20 21:11:48 +0200

    more monads

--- Monads.hs
+++ Monads.hs
@@ -4,4 +4,6 @@ module Monands where
         (>>=) :: m a -> (a -> m b) -> m b
         return :: a -> m a
 
-
+    class Functor m => MonadAlt m where
+        join :: m(m a) -> m a
+        returnAlt :: a -> m a

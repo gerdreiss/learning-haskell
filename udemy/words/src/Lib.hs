ec7a09bf2bb99bb270923fd2dc2874e3eb2df5f7
gerdreiss <gerdreiss@jscriptive.com>
2018-05-30 21:02:35 +0200

    TESTING

--- udemy/words/src/Lib.hs
+++ udemy/words/src/Lib.hs
@@ -1,44 +1,16 @@
 module Lib
-    ( grid
-    , languages
-    , formatGrid
+    ( formatGrid
     , outputGrid
+    , skew
     , findWordInLine
     , findWord
-    . findWords
+    , findWords
     ) where
 
 import Data.List (isInfixOf, transpose)
 import Data.Maybe (catMaybes)
 
 type Grid = [String]
-
-
-grid = [ "__C________R___"
-       , "__SI________U__"
-       , "__HASKELL____B_"
-       , "__A__A_____S__Y"
-       , "__R___B___C____"
-       , "__PHP____H_____"
-       , "____S_LREP_____"
-       , "____I_M__Y__L__"
-       , "____L_E__T_O___"
-       , "_________HB____"
-       , "_________O_____"
-       , "________CN_____"
-       ]
-
-languages = [ "BASIC"
-            , "COBOL"
-            , "CSHARP"
-            , "HASKELL"
-            , "LISP"
-            , "PERL"
-            , "PHP"
-            , "PYTHON"
-            , "RUBY"
-            , "SCHEME"
-            ]
 
 
 outputGrid :: Grid -> IO ()
--- udemy/words/test/Spec.hs
+++ udemy/words/test/Spec.hs
@@ -1,8 +1,22 @@
 import Test.Hspec
 import Lib
+import Data
 
 main :: IO ()
 main = hspec $ do
-    describe "How to write a test" $ do
-        it "Should be able to run tests" $ do
+    describe "formatGrid" $ do
+        it "Should concatenate everyline with a newline" $ do
             formatGrid grid `shouldBe` unlines grid
+
+    describe "findWord" $ do
+        it "Should find words that exist on the Grid" $ do
+            findWord grid "HASKELL" `shouldBe` Just "HASKELL"
+            findWord grid "PERL" `shouldBe` Just "PERL"
+        it "Should not find words that do not exist on the Grid" $ do
+            findWord grid "SCALA" `shouldBe` Nothing
+
+    describe "findWords" $ do
+        it "Should find all the words that exist on the Grid" $ do
+            findWords grid languages `shouldBe` languages
+        it "Should not find the words that do not exist on the Grid" $ do
+            findWords grid ["SCALA", "JAVA"] `shouldBe` []

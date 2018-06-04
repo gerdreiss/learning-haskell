884c3c84346d6c4a5d2436fc0d25aa0d9a574230
gerdreiss <gerdreiss@jscriptive.com>
2018-06-04 20:40:38 +0200

    fixed cases

--- futurelearn/cases.hs
+++ futurelearn/cases.hs
@@ -1,9 +1,9 @@
 
 data Pet = Cat | Dog | Fish | Parrot String
 
-hello : :Pet -> String
-hello x = 
-    case x of
+hello :: Pet -> String
+hello pet = 
+    case pet of
         Cat -> "meow"
         Dog -> "woof"
         Fish -> "bubble"

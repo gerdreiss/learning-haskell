873744b61e87b2f9237a0c963e0bf8326dd7c910
Reiss Gerd <Gerd.Reiss@finnova.com>
2018-06-05 14:25:02 +0200

    Verbose Test Cipher

--- futurelearn/cipher/test/Spec.hs
+++ futurelearn/cipher/test/Spec.hs
@@ -6,4 +6,4 @@ import           Test.QuickCheck
 testcipher = (\n -> (\s -> ((decipher n (cipher n s)) == s))) :: Int -> [Char] -> Bool
 
 main :: IO ()
-main = quickCheck testcipher
+main = verboseCheck testcipher

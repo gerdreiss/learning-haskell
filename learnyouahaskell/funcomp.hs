745111a9a706d558d8d9d0a06740ba7c831ed03f
Reiss Gerd <Gerd.Reiss@finnova.com>
2018-06-12 11:45:15 +0200

    start with 0

--- learnyouahaskell/funcomp.hs
+++ learnyouahaskell/funcomp.hs
@@ -10,4 +10,4 @@ digitSum :: Int -> Int
 digitSum = sum . map digitToInt . show
 
 firstTo :: Int -> Maybe Int
-firstTo n = find (\x -> digitSum x == n) [1..]
+firstTo n = find (\x -> digitSum x == n) [0..]

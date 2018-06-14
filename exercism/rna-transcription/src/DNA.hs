c701e9724de1df5be6e36451b298e14f71fa1151
Reiss Gerd <Gerd.Reiss@finnova.com>
2018-06-14 16:04:36 +0200

    comments

--- exercism/rna-transcription/src/DNA.hs
+++ exercism/rna-transcription/src/DNA.hs
@@ -7,13 +7,13 @@ toRNA = traverse (`lookup` [('C', 'G'), ('G', 'C'), ('T', 'A'), ('A', 'U')])
 --      traverse r2d
 --      allOrNothing . map r2d
 
-
+-- Unnecessary function because of traverse
 allOrNothing :: [Maybe Char] -> Maybe String
 allOrNothing xs
     | Nothing `elem` xs = Nothing
     | otherwise         = Just $ catMaybes xs
 
-
+-- Unnecessary function because of lookup
 r2d :: Char -> Maybe Char
 r2d d =
     -- Using case of

5a99d607431cb332abc2b3dc6263db4d044565f2
Gerd Reiss <gerdreiss@jscriptive.com>
2018-06-16 18:15:16 +0200

    Списки: Функции высших порядков - Enum Odd

---
+++ stepik/part1/src/EnumOdd.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module EnumOdd
+  ( Odd(..)
+  ) where
+
+data Odd =
+  Odd Integer
+  deriving (Eq, Show)
+
+instance Enum Odd where
+  toEnum i = Odd (toInteger i)
+  fromEnum (Odd n) = fromEnum n
+  succ (Odd n) = Odd (n + 2)
+  pred (Odd n) = Odd (n - 2)
+  enumFrom (Odd n) = map Odd [n,n + 2 ..]
+  enumFromTo (Odd n) (Odd m) = map Odd [n,n + 2 .. m]
+  enumFromThen (Odd n) (Odd n') = map Odd [n,n' ..]
+  enumFromThenTo (Odd n) (Odd n') (Odd m) = map Odd [n,n' .. m]
+--   succ (Odd x)     = Odd (toInteger $ fromIntegral(x) + 2)
+--   pred (Odd x)     = Odd (toInteger $ fromIntegral(x) - 2)
+--   toEnum n         = Odd (toInteger $ 2 * n + 1)
+--   fromEnum (Odd x) = fromInteger $ (x - 1) `div` 2

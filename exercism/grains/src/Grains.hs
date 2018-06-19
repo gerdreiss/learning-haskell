05ebf09ffae46165c37e2767737bb59eea8522bd
Reiss Gerd <Gerd.Reiss@finnova.com>
2018-06-19 08:34:55 +0200

    Exercism: Grains on a chess board

---
+++ exercism/grains/src/Grains.hs
@@ -0,0 +1,9 @@
+module Grains (square, total) where
+
+square :: Integer -> Maybe Integer
+square n
+  | n `elem` [1..64] = Just $ 2^(n-1)
+  | otherwise        = Nothing
+
+total :: Integer
+total = maybe 0 id . fmap sum . sequence . map square $ [1..64]

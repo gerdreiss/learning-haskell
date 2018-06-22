35ca5e9d56c6df763face39c18e64499babb6d0a
Reiss Gerd <Gerd.Reiss@finnova.com>
2018-06-22 08:17:18 +0200

    Stepik: Functor for Entry Map

---
+++ stepik/part1/src/FEntryMap.hs
@@ -0,0 +1,13 @@
+module FEntryMap (Entry(..), Map(..)) where
+
+data Entry k1 k2 v = Entry (k1, k2) v  deriving Show
+data Map k1 k2 v = Map [Entry k1 k2 v]  deriving Show
+
+
+
+instance Functor (Entry k1 k2) where
+    fmap f (Entry (k1, k2) v) = Entry (k1, k2) (f v)
+
+instance Functor (Map k1 k2) where
+    fmap f (Map []) = Map []
+    fmap f (Map es) = Map (map (fmap f) es)

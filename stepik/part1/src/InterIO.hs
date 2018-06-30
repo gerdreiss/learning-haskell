5c51faa301e92e140029b77fbf866b4978b95c5c
gerdreiss <gerdreiss@jscriptive.com>
2018-06-30 13:50:12 +0200

    IO Monad: more code for inspiration

--- stepik/part1/src/InterIO.hs
+++ stepik/part1/src/InterIO.hs
@@ -19,3 +19,31 @@ main' = do
     delete deletable = do
       putStrLn $ "Removing file: " ++ deletable
       removeFile $ "./test/" ++ deletable
+
+
+-- nicer, not mine
+main'' :: IO ()
+main'' = do
+    putStr "Substring: "
+    substr <- getLine
+    if null substr then putStrLn "Canceled"
+    else do
+        filtered <- getDirectoryContents "." >>= return . filter (isInfixOf substr)
+        mapM_ putStrLn . map (\x -> "Removing file: " ++ x) $ filtered
+        mapM_ removeFile filtered
+
+-- also nice, also not mine
+-- check <$> that is used instead of >>= return .
+deleteMessage = ("Removing file: "++)
+deleteFun path = putStrLn (deleteMessage path) >> removeFile path
+
+main''' :: IO ()
+main''' = do
+    putStr "Substring: "
+    subs <- getLine
+    case subs of
+        [] -> putStrLn "Canceled"
+        _ -> do
+            files <- filter (isInfixOf subs) <$> getDirectoryContents "."
+            mapM_ deleteFun files
+

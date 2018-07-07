58e932d51788e37d3a6101244373d6b64a3de7bf
gerdreiss <gerdreiss@jscriptive.com>
2018-07-07 16:37:53 +0200

    using withFile

---
+++ learnyouahaskell/wordsWithFile.hs
@@ -0,0 +1,7 @@
+import System.IO
+
+main = do
+    withFile "words.txt" ReadMode (\handle -> do
+                                        contents <- hGetContents handle
+                                        putStr contents)
+

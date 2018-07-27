d58b5e79a96995e0e7b192d06fd5c200ecf1815f
Reiss Gerd <Gerd.Reiss@finnova.com>
2018-07-27 07:53:57 +0200

    Exercism.io, Haskell Track, Hamming, alternative implementation

--- exercism/hamming/src/Hamming.hs
+++ exercism/hamming/src/Hamming.hs
@@ -6,3 +6,8 @@ distance :: String -> String -> Maybe Int
 distance xs ys =
   if length xs /= length ys then Nothing
   else Just . length . filter id $ zipWith (/=) xs ys
+
+distance0 :: String -> String -> Maybe Int
+distance0 xs ys =
+  if length xs /= length ys then Nothing
+  else Just $ length $ filter (uncurry (/=)) (xs `zip` ys)

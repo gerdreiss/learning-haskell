f9e6f6e9c427d243792eb4373332d2ad65ab0d03
Gerd Reiss <gerdreiss@jscriptive.com>
2018-07-31 23:37:09 +0200

    comments

--- learnyouahaskell/todo.hs
+++ learnyouahaskell/todo.hs
@@ -11,6 +11,10 @@ main = do
         (cmd:args) -> dispatch cmd args
         _          -> what []
 
+{-
+   invokes the function with the name given in the first parameter,
+   and the rest parameters
+-}
 dispatch :: String -> [String] -> IO ()
 dispatch "add"    = add
 dispatch "edit"   = edit
@@ -19,6 +23,9 @@ dispatch "remove" = remove
 dispatch "help"   = help
 dispatch _        = what
 
+{-
+   prints out help
+-}
 help :: [String] -> IO ()
 help _ = putStrLn "Usage: \n\
                   \Add new TODO item  :  todo add <file> <text>\n\
@@ -27,13 +34,22 @@ help _ = putStrLn "Usage: \n\
                   \Remove a TODO item :  todo remove <file> <num>\n\
                   \Show this text     :  todo help\n"
 
+{-
+   prints out an error message
+-}
 what :: [String] -> IO ()
 what _ = print "Um, no, I don't understand... Try with todo help"
 
+{-
+   appends a new line to the given file
+-}
 add :: [String] -> IO ()
 add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
 add _                    = what []
 
+{-
+   edits the line with the given number
+-}
 edit :: [String] -> IO ()
 edit [fileName, numberString, newText] = do
     contents <- readFile fileName
@@ -42,6 +58,9 @@ edit [fileName, numberString, newText] = do
     _override fileName newTodoItems
 edit _ = what []
 
+{-
+   displays the contents of the file with the given name
+-}
 view :: [String] -> IO ()
 view [fileName] = do
     contents <- readFile fileName
@@ -50,6 +69,9 @@ view [fileName] = do
     putStr $ unlines numberedTasks
 view _ = what []
 
+{-
+   removes the line with the given number
+-}
 remove :: [String] -> IO ()
 remove [fileName, numberString] = do
     contents <- readFile fileName
@@ -58,12 +80,20 @@ remove [fileName, numberString] = do
     _override fileName newTodoItems
 remove _ = what []
 
+{-
+   reads lines from the given string,
+   remove the line with the given number,
+   and returns the removed line number and the resulting lines
+-}
 _linesSkippingLine :: String -> String -> (Int, [String])
 _linesSkippingLine contents numberString = (number, reduced)
      where number = read numberString
            todos = lines contents
            reduced = delete (todos !! (number - 1)) todos
 
+{-
+   overrides the contents of the file with the given name with the given new contents
+-}
 _override :: String -> String -> IO ()
 _override fileName todoItems =
     bracketOnError (openTempFile "." "temp")

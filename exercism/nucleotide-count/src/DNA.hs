7858bc6520f53e750a356126d3a855586f34b711
Gerd Reiss <gerdreiss@jscriptive.com>
2018-08-04 16:45:10 +0200

    Nucleotide count, version 0.1

---
+++ exercism/nucleotide-count/src/DNA.hs
@@ -0,0 +1,11 @@
+module DNA (nucleotideCounts) where
+
+import           Data.Map (Map)
+import qualified Data.Map as Map
+
+
+nucleotideCounts :: String -> Either String (Map Char Int)
+nucleotideCounts cs
+  | all (`elem` "ACGT") cs = Right $ Map.fromList $ map (\c -> (c, length . filter (== c) $ cs)) "ACGT"
+  | otherwise              = Left "Empty"
+

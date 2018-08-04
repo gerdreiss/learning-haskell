9c29492df05acd902934e42fc8a5d8ced68e9b8f
gerdreiss <gerdreiss@jscriptive.com>
2018-08-04 16:50:55 +0200

    better error message

--- exercism/nucleotide-count/src/DNA.hs
+++ exercism/nucleotide-count/src/DNA.hs
@@ -7,5 +7,5 @@ import qualified Data.Map as Map
 nucleotideCounts :: String -> Either String (Map Char Int)
 nucleotideCounts cs
   | all (`elem` "ACGT") cs = Right $ Map.fromList $ map (\c -> (c, length . filter (== c) $ cs)) "ACGT"
-  | otherwise              = Left "Empty"
+  | otherwise              = Left "Invalid strand"
 

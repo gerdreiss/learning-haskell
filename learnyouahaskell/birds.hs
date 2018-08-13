c29e5ef2702688d6996365ca6e047018d869593b
gerdreiss <gerdreiss@jscriptive.com>
2018-08-13 19:43:20 +0200

    Monads

---
+++ learnyouahaskell/birds.hs
@@ -0,0 +1,20 @@
+
+
+
+main :: IO ()
+main = print $ return (0, 0) >>= landRight 1 >>= landLeft 1 >>= landRight 2 >>= landLeft 1
+
+
+type Birds = Int
+type Pole = (Birds, Birds)
+
+landLeft :: Birds -> Pole -> Maybe Pole
+landLeft n (left, right)
+  | abs ((left + n) - right) < 4 = Just (left + n, right)
+  | otherwise = Nothing
+
+landRight :: Birds -> Pole -> Maybe Pole
+landRight n (left, right)
+  | abs (left - (right + n)) < 4 = Just (left, right + n)
+  | otherwise = Nothing
+

7e846133a7f5300f5afeb2bcaff8aad531199063
Gerd Reiss <gerdreiss@jscriptive.com>
2019-02-07 20:33:36 +0100

    formatting

--- realworld/ch07/tempfile.hs
+++ realworld/ch07/tempfile.hs
@@ -11,17 +11,24 @@ action :: FilePath -> Handle -> IO ()
 action tempname temph = do
   putStrLn "Welcome to tempfile.hs"
   putStrLn $ "I have a temporary file at " ++ tempname
+
   pos <- hTell temph
   putStrLn $ "My initial position is " ++ show pos
+
   let tempdata = show [1 .. 10]
+
   putStrLn $
     "Writing one line containing " ++
     show (length tempdata) ++ " bytes: " ++ tempdata
+
   hPutStrLn temph tempdata
+
   pos <- hTell temph
+
   putStrLn $ "After wrinting, my new position is " ++ show pos
   putStr "The file content is: "
   hSeek temph AbsoluteSeek 0
+
   c <- hGetContents temph
   putStrLn c
   putStrLn "Which could be expressed as this Haskell literal: "

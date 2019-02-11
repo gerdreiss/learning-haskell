1c2f42def928fdf6140b6c572c015ffabead9297
Gerd Reiss <gerdreiss@jscriptive.com>
2019-02-11 20:34:26 +0100

    reformat

--- realworld/ch07/callingpure.hs
+++ realworld/ch07/callingpure.hs
@@ -7,7 +7,6 @@ main = do
 
 name2reply :: String -> String
 name2reply name =
-  "Pleased to meet you, " ++
-  name ++ ".\n" ++ "Your name contains " ++ charcount ++ " characters."
-  where
-    charcount = show $ length name
+  "Pleased to meet you, " ++ name ++ ".\n" ++
+  "Your name contains " ++ cc ++ " characters."
+  where cc = show $ length name

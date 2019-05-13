7a6264665d7342fbfaa284c4ed6f9452ef2f382a
Gerd Reiss <gerdreiss@jscriptive.com>
2019-05-13 19:53:31 +0200

    move code to project/lib

--- GPWH/lesson23/sanscrit/src/Sanscrit.hs
+++ GPWH/lesson23/sanscrit/src/Sanscrit.hs
@@ -1,23 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Sanscrit (run) where
+module Sanscrit
+  ( run
+  ) where
 
 import qualified Data.Text    as T
 import qualified Data.Text.IO as TIO
 
-
-dharma :: T.Text
-dharma :: "धर्म"
-
-bgText :: T.Text
-bgText = "श्रेयान्स्वधर्मो विगुणः परधर्मात्स्वनुष्ठितात्। स्वधर्मे निधनं श्रेयः परधर्मो भयावहः"
-
 highlight :: T.Text -> T.Text -> T.Text
 highlight query fullText = T.intercalate highlighted pieces
-  where pieces = T.splitOn query fullText
-        highlighted = mconcat ["{",query,"}"]
-
+  where
+    highlighted = mconcat ["{", query, "}"]
+    pieces = T.splitOn query fullText
 
 run :: IO ()
 run = do
-  TIO.putStrLn (highlight dharma bgText)
+  TIO.putStrLn (highlight "धर्म" "श्रेयान्स्वधर्मो विगुणः परधर्मात्स्वनुष्ठितात्। स्वधर्मे निधनं श्रेयः परधर्मो भयावहः")

0e04dbd33cec0420d480ecd1e8dde8a2d26a876c
Gerd Reiss <gerdreiss@users.noreply.github.com>
2019-05-13 19:16:37 +0200

    copy text from...
    ...https://gist.github.com/willkurt/4bced09adc2ff9e7ee366b7ad681cac6

--- GPWH/lesson23/bg_highlight.hs
+++ GPWH/lesson23/bg_highlight.hs
@@ -4,10 +4,10 @@ import qualified Data.Text    as T
 import qualified Data.Text.IO as TIO
 
 dharma :: T.Text
-dharma :: "धर्"
+dharma :: "धर्म"
 
 bgText :: T.Text
-bgText = "श्रेयान्स्वधर्मो विगुणः परधर्मात् वनुष्ठितात् स्वध निधनं श्रे परधर् भयाव"
+bgText = "श्रेयान्स्वधर्मो विगुणः परधर्मात्स्वनुष्ठितात्। स्वधर्मे निधनं श्रेयः परधर्मो भयावहः"
 
 highlight :: T.Text -> T.Text -> T.Text
 highlight query fullText = T.intercalate highlighted pieces

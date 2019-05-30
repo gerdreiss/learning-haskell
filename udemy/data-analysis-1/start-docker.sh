0bf58ce43ed382e82b57db5ee4bc7379128a40e3
Gerd Reiss <gerdreiss@jscriptive.com>
2019-05-30 13:11:16 +0200

    docker container names

--- udemy/data-analysis-1/start-docker.sh
+++ udemy/data-analysis-1/start-docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker build -t mine/ihaskell .
+docker build -t grei/ihaskell .
 
 docker run -d \
     --rm -p 8888:8888 \
     --volume `pwd`/work:/home/jovyan/work \
     --env JUPYTER_ENABLE_LAB=yes \
     --env JUPYTER_TOKEN=x \
-    --name ihaskell mine/ihaskell:latest
+    --name ihaskell grei/ihaskell:latest

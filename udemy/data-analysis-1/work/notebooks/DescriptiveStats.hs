44fc5c6a26cc8e73c7af9c6e903aa369e0388285
Gerd Reiss <gerdreiss@jscriptive.com>
2019-06-04 19:12:58 +0200

    Haskell: Data Analysis Made Easy, Section 4: Regex + DescriptiveStats.hs

---
+++ udemy/data-analysis-1/work/notebooks/DescriptiveStats.hs
@@ -0,0 +1,61 @@
+module DescriptiveStats where
+
+import Text.CSV
+import Data.Maybe
+import Data.List
+import Data.Ord
+
+noEmptyRows :: Either a CSV -> CSV
+noEmptyRows ecsv = either (const []) (filter (\row -> 2 <= length row)) ecsv
+
+{- Reads a column of data from a CSV file -}
+readIndex :: Read cell => Either a CSV -> Int -> [cell]
+readIndex ecsv index = map read (getIndex ecsv index)
+
+{- Reads a column of data from a CSV file -}
+getIndex :: Either a CSV -> Int -> [String]
+getIndex ecsv index = map (!! index) (noEmptyRows ecsv)
+
+range :: Ord a => [a] -> Maybe (a, a)
+range []  = Nothing
+range [x] = Just (x, x)
+range xs  = Just (minimum xs, maximum xs)
+
+mean :: Real a => [a] -> Maybe Double
+mean []  = Nothing
+mean [x] = Just $ realToFrac x
+mean xs  = Just $ realToFrac (sum xs) / fromIntegral (length xs)
+
+stdev :: Real a => [a] -> Maybe Double
+stdev []  = Nothing
+stdev [_] = Nothing
+stdev xs  = Just $ sqrt (sumsquares / n_m1)
+  where
+    sumsquares   = sum $ map (diffsquare . realToFrac) xs
+    diffsquare x = (x - meanxs) * (x - meanxs)
+    meanxs       = fromJust (mean xs)
+    n_m1         = fromIntegral (length xs - 1)
+
+median :: Real a => [a] -> Maybe Double
+median [] = Nothing
+median list
+  | odd (length list) = Just middleValue
+  | otherwise         = Just middleEven
+  where
+    sorted            = sort list
+    middleIndex       = length list `div` 2
+    middleValue       = realToFrac $ sorted !! middleIndex
+    beforeMiddleValue = realToFrac $ sorted !! (middleIndex - 1)
+    middleEven        = 0.5 * (middleValue + beforeMiddleValue)
+
+runLengthEncoding :: Ord a => [a] -> [(a, Integer)]
+runLengthEncoding = map (\xs -> (head xs, genericLength xs)) . group
+
+
+mode :: Ord a => [a] -> Maybe (a, Integer)
+mode [] = Nothing
+mode list = Just $ maximumBy (comparing snd) pairs
+  where
+    sorted = sort list
+    pairs = runLengthEncoding sorted
+

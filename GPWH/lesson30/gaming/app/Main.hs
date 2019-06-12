d536ad35c7061a21546a656854bb9216bf067b75
Gerd Reiss <gerdreiss@jscriptive.com>
2019-06-12 20:38:48 +0200

    Get Programming with Haskell, Lesson 30, Monads

--- GPWH/lesson30/gaming/app/Main.hs
+++ GPWH/lesson30/gaming/app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
-import Gaming
+import           Gaming
+import           System.IO (hFlush, stdout)
 
 main :: IO ()
-main = putStrLn "Gaming FTW!"
+main = putStrLn "What is your name?" >>
+       hFlush stdout >>
+       getLine >>=
+       nameStatement >>=
+       putStrLn >>
+       putStrLn "Welcome!"
+
+nameStatement :: String -> IO String
+nameStatement name = return ("Hello, " ++ name ++ "!")

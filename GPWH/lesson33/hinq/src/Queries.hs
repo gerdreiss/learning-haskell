60699b1f2aae29d54fd0faece1a6d82010665328
Gerd Reiss <gerdreiss@jscriptive.com>
2019-06-18 19:40:06 +0200

    Get Programming with Haskell, Lesson 33, Capstone, SQL-like queries

--- GPWH/lesson33/hinq/src/Queries.hs
+++ GPWH/lesson33/hinq/src/Queries.hs
@@ -26,13 +26,12 @@ startsWithS prefix string
   | length prefix > length string = False
   | otherwise = startsWith (head prefix) string && startsWithS (tail prefix) (tail string)
 
-_join0 :: Eq c => [a] -> [b] -> (a -> c) -> (b -> c) -> [(a, b)]
-_join0 data1 data2 prop1 prop2 = do
-  d1 <- data1
-  d2 <- data2
-  let dpairs = (d1, d2)
-  guard ((prop1 . fst $ dpairs) == (prop2 . snd $ dpairs))
-  return dpairs
-
+--_join0 :: Eq c => [a] -> [b] -> (a -> c) -> (b -> c) -> [(a, b)]
+--_join0 data1 data2 prop1 prop2 = do
+--  d1 <- data1
+--  d2 <- data2
+--  let dpairs = (d1, d2)
+--  guard ((prop1 . fst $ dpairs) == (prop2 . snd $ dpairs))
+--  return dpairs
 _join :: Eq c => [a] -> [b] -> (a -> c) -> (b -> c) -> [(a, b)]
 _join data1 data2 prop1 prop2 = [(d1, d2) | d1 <- data1, d2 <- data2, prop1 d1 == prop2 d2]

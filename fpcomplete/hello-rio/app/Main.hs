0046bdcdce205e8649e1195a4c82ec9cbf2c7336
Gerd Reiss <gerdreiss@jscriptive.com>
2019-07-10 19:46:37 +0200

    fpcomplete RIO standard lib

--- fpcomplete/hello-rio/app/Main.hs
+++ fpcomplete/hello-rio/app/Main.hs
@@ -1,44 +1,52 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import RIO
-import RIO.Time (getCurrentTime)
-import System.IO (hPutStrLn, stderr)
+import           RIO
+import           RIO.Time  (getCurrentTime)
+import           System.IO (hPutStrLn, stderr, stdout)
 
 data App = App
-  { appName :: !String
-  , appHandle :: !Handle
-  }
+    { appName   :: !String
+    , appHandle :: !Handle
+    }
+
+class HasHandle env where
+  getHandle :: env -> Handle
+
+instance HasHandle Handle where
+  getHandle = id
+
+instance HasHandle App where
+  getHandle = appHandle
 
 main :: IO ()
 main = do
-  let app = App
-        { appName = "Alice"
-        , appHandle = stderr
-        }
+  let app = App { appName = "Alice", appHandle = stderr }
   runRIO app $ do
     sayHello
     sayTime
     sayGoodbye
+  -- Also works!
+  runRIO stdout sayTime
 
-say :: String -> RIO App ()
+say :: HasHandle env => String -> RIO env ()
 say msg = do
-  App _name h <- ask
-  liftIO $ hPutStrLn h msg
+  env <- ask
+  liftIO $ hPutStrLn (getHandle env) msg
+
+sayTime :: HasHandle env => RIO env ()
+sayTime = do
+  now <- getCurrentTime
+  say $ "The time is: " ++ show now
 
 sayHello :: RIO App ()
 sayHello = do
   App name _h <- ask
   say $ "Hello, " ++ name
 
-sayTime :: RIO App ()
-sayTime = do
-  now <- getCurrentTime
-  say $ "The time is: " ++ show now
-
 sayGoodbye :: RIO App ()
 sayGoodbye = do
   App name _h <- ask
   say $ "Goodbye, " ++ name
-

9d293a580048e14be73a98717c76b32fb2df28ea
Gerd Reiss <gerdreiss@jscriptive.com>
2019-07-22 21:28:14 +0200

    Haskell in Depth, Chapter 2, basic text processing

--- HID/shakespeare/app/Main.hs
+++ HID/shakespeare/app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import           Shakespeare        (processTextFile)
+
 --import           Data.Char
 --import           Data.List
 --import qualified Data.Text          as T
@@ -13,8 +14,14 @@ import           System.Environment
 --  let ws = map head . group . sort . map T.toCaseFold . filter (not . T.null) . map (T.dropAround $ not . isLetter) . T.words $ text
 --  TIO.putStrLn $ T.unwords ws
 --  print $ length ws
+--main = do
+--  args <- getArgs
+--  case args of
+--    [fname] -> processTextFile fname
+--    _       -> putStrLn "Usage: vocab-builder filename"
+main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [fname] -> processTextFile fname
-    _       -> putStrLn "Usage: vocab-builder filename"
+    [fname, n] -> processTextFile fname (read n)
+    _ -> putStrLn "Usage: vocab-builder filename number_of_frequent_words"
--- HID/shakespeare/src/Shakespeare.hs
+++ HID/shakespeare/src/Shakespeare.hs
@@ -1,34 +1,48 @@
-module Shakespeare (processTextFile) where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Shakespeare
+  ( processTextFile
+  ) where
 
 import           Data.Char
 import           Data.List
-import qualified Data.Text    as T
-import qualified Data.Text.IO as TIO
+import           Data.Ord
+import qualified Data.Text          as T
+import qualified Data.Text.IO       as TIO
+import           System.Environment
 
-type Entry = (T.Text, Int) -- one entry
-
-type Vocabulary = [Entry] -- a list of entries
+type Entry = (T.Text, Int)
+type Vocabulary = [Entry]
 
 extractVocab :: T.Text -> Vocabulary
-extractVocab text = map buildEntry . group . sort $ ws
+extractVocab t = map buildEntry . group . sort $ ws
   where
-    ws = map T.toCaseFold . filter (not . T.null) . map cleanWord . T.words $ text
+    ws = map T.toCaseFold . filter (not . T.null) . map cleanWord . T.words $ t
     buildEntry ws@(w:_) = (w, length ws)
     cleanWord = T.dropAround (not . isLetter)
 
-printAllWords :: Vocabulary -> IO ()
-printAllWords vocab = do
-  putStrLn "All words: "
-  TIO.putStrLn . T.unlines . map fst $ vocab
+allWordsReport :: Vocabulary -> T.Text
+allWordsReport vocab = T.append "\nAll words:\n" $ T.unlines $ map fst vocab
 
-processTextFile :: FilePath -> IO ()
-processTextFile fname = do
+wordsCount :: Vocabulary -> Int
+wordsCount vocab = sum $ map snd vocab
+
+wordsCountReport :: Vocabulary -> T.Text
+wordsCountReport vocab = T.append "\nTotal number of words: " $ T.pack $ show $ wordsCount vocab
+
+wordsByFrequency :: Vocabulary -> Vocabulary
+--wordsByFrequency = sortBy (comparing $ Down . snd)
+wordsByFrequency = sortOn (Down . snd)
+
+frequentWordsReport :: Vocabulary -> Int -> T.Text
+frequentWordsReport vocab n = T.append "\nFrequent words:\n" . T.unlines . map showEntry . take n . wordsByFrequency $ vocab
+  where
+    showEntry (t, n) = T.append t $ T.pack $ " - " ++ show n
+
+processTextFile :: FilePath -> Int -> IO ()
+processTextFile fname n = do
   text <- TIO.readFile fname
   let vocab = extractVocab text
-  printAllWords vocab
-
-printWordsCount :: Vocabulary -> IO ()
-printWordsCount = print
-
-printFrequentWords :: Vocabulary -> Int -> IO ()
-printFrequentWords vocab frequency = printWordsCount $ filter (\v -> snd v <= frequency) vocab
+  TIO.putStrLn $ allWordsReport vocab
+  TIO.putStrLn $ wordsCountReport vocab
+  TIO.putStrLn $ frequentWordsReport vocab n

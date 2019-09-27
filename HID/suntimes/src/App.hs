8c7181c211a6f067e049c57daa88720ef2e50f26
Reiss Gerd <Gerd.Reiss@finnova.com>
2019-09-27 10:48:07 +0200

    Haskell in Depth, Chapter 7, sun times

--- HID/suntimes/src/App.hs
+++ HID/suntimes/src/App.hs
@@ -1,2 +1,27 @@
 module App where
 
+import           Options.Applicative
+
+data AppMode = FileInput FilePath | Interactive
+data Params = Params
+                AppMode -- mode
+                FilePath -- config file
+
+mkParams :: Parser Params
+mkParams = Params <$> (fileInput <|> interactive) <*> config
+ where
+  fileInput = FileInput <$> strOption
+    (long "file" <> short 'f' <> metavar "FILENAME" <> help "Input file")
+  interactive = flag
+    Interactive
+    Interactive
+    (long "interactive" <> short 'i' <> help "Interactive mode")
+  config = strOption
+    (  long "conf"
+    <> short 'c'
+    <> value "config.json"
+    <> showDefault
+    <> metavar "CONFIGNAME"
+    <> help "Configuration file"
+    )
+
--- HID/suntimes/src/GeoCoordsReq.hs
+++ HID/suntimes/src/GeoCoordsReq.hs
@@ -1,2 +1,56 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module GeoCoordsReq where
 
+import           Data.Aeson
+import           Data.Text
+import           Data.Text.Encoding
+import           GHC.Generics
+import           Network.HTTP.Req
+import           Control.Monad.Reader           ( ReaderT
+                                                , MonadIO
+                                                , MonadReader
+                                                , runReaderT
+                                                )
+
+type Address = Text
+
+data GeoCoords = GeoCoords
+  { lat :: Text
+  , lon :: Text
+  } deriving (Show, Generic)
+
+instance FromJSON GeoCoords
+
+data WebAPIAuth = WebAPIAuth
+  { timeZoneDBkey :: Text
+  , email :: Text
+  , agent :: Text
+  } deriving (Generic, Show)
+
+instance FromJSON WebAPIAuth
+
+newtype MyApp a = MyApp {
+      runApp :: ReaderT WebAPIAuth IO a
+    } deriving (Functor, Applicative, Monad, MonadIO, MonadReader WebAPIAuth)
+
+runMyApp :: MyApp a -> WebAPIAuth -> IO a
+runMyApp app = runReaderT (runApp app)
+
+getCoords :: Address -> WebAPIAuth -> IO GeoCoords
+getCoords addr wauth = do
+  let url       = https "nominatim.openstreetmap.org" /: "search"
+      reqParams = mconcat
+        [ "q" =: addr
+        , "format" =: ("json" :: Text)
+        , "limit" =: (1 :: Int)
+        , "email" =: email wauth
+        , header "User-Agent" (encodeUtf8 $ agent wauth)
+        ]
+      request = req GET url NoReqBody jsonResponse reqParams
+  res <- responseBody <$> runReq defaultHttpConfig request
+  case res of
+    []           -> pure GeoCoords { lat = "", lon = "" } -- TODO: this is a no answer situation
+    (coords : _) -> pure coords

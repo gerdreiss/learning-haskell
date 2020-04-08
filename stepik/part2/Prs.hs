524a951770b4033d3578efd4293f990c28c2d2b5
Gerd Reiss <gerdreiss@jscriptive.com>
2020-04-08 19:34:02 +0200

    Аппликативный парсер своими руками

--- stepik/part2/Prs.hs
+++ stepik/part2/Prs.hs
@@ -1,15 +1,40 @@
 module Prs where
 
-newtype Prs a = Prs { runPrs :: String -> Maybe (a, String) }
+import           Control.Applicative hiding (many)
+import           Data.Char
+
+newtype Prs a = Prs
+ { runPrs :: String -> Maybe (a, String) }
 
 instance Functor Prs where
-     -- fmap :: (a -> b) -> Prs a -> Prs b
-        fmap f p = Prs fun where
-                fun s = case runPrs p s of
-                    Nothing      -> Nothing
-                    Just (a, s') -> Just (f a, s')
+--fmap :: (a -> b) -> Prs a -> Prs b
+  fmap f p = Prs fun where
+    fun s = case runPrs p s of
+      Nothing      -> Nothing
+      Just (a, s') -> Just (f a, s')
+
+-- this is mine
+-- instance Applicative Prs where
+--   pure a = Prs fun where
+--     fun s = Just (a, s)
+--   pf <*> pv = Prs fun where
+--     fun s = case runPrs pf s of
+--       Nothing -> Nothing
+--       Just (f, s') -> case runPrs pv s' of
+--         Nothing       -> Nothing
+--         Just (a, s'') -> Just (f a, s'')
+
+-- but this is much nicer
+instance Applicative Prs where
+  pure a = Prs func where
+    func s = return (a, s)
+  pf <*> pv = Prs func where
+    func s = do
+      (g, s')  <- runPrs pf s
+      (a, s'') <- runPrs pv s'
+      return (g a, s'')
 
 anyChr :: Prs Char
 anyChr = Prs f where
-        f []     = Nothing
-        f (c:cs) = Just (c, cs)
+  f []     = Nothing
+  f (c:cs) = Just (c, cs)

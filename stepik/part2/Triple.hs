05f14c7456819c1a2bbc3ebd8398e70d68278a38
Gerd Reiss <gerdreiss@jscriptive.com>
2020-04-25 11:43:19 +0200

    Класс типов Foldable

--- stepik/part2/Triple.hs
+++ stepik/part2/Triple.hs
@@ -10,3 +10,13 @@ instance Functor Triple where
 instance Applicative Triple where
   pure a = Tr a a a
   Tr f g h <*> Tr a b c = Tr (f a) (g b) (h c)
+
+-- my solution
+-- instance Foldable Triple where
+--   foldr f ini (Tr a b c) = f a . f b . f c $ ini
+--   foldl f ini (Tr a b c) = f (f (f ini a) b) c
+
+-- an unexpectedly elegant, and a litle cheaty, solution from somebody else ;)
+instance Foldable Triple where
+  foldr f ini (Tr a b c) = foldr f ini [a, b, c]
+  foldl f ini (Tr a b c) = foldl f ini [a, b, c]

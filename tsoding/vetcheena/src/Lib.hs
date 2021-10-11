adc6bb71a5f991260797466aefe07d3a4c1c164f
Gerd Reiss <gerd@reiss.pro>
2021-10-11 17:00:18 +0200

    Tsoding: Spam Filter in Haskell

--- tsoding/vetcheena/src/Lib.hs
+++ tsoding/vetcheena/src/Lib.hs
@@ -8,10 +8,12 @@ import           Data.Char                      ( isAlphaNum )
 import           Data.Foldable                  ( Foldable(fold) )
 import           Data.Maybe                     ( fromMaybe
                                                 , isJust
+                                                , mapMaybe
                                                 )
 import           System.Directory               ( listDirectory )
 
 import           Prelude                 hiding ( Word )
+
 
 newtype Word = Word T.Text deriving (Show, Read, Eq, Ord)
 newtype Bow  = Bow { bowToMap :: M.Map Word Int } deriving (Show)
@@ -22,24 +24,19 @@ instance Semigroup Bow where
 instance Monoid Bow where
   mempty = Bow M.empty
 
-data SpamClassifier = SpamClassifier
+
+data SpamModel = SpamModel
   { spam :: Bow
   , ham  :: Bow
   }
   deriving Show
 
-loadSpamClassifier :: IO SpamClassifier
-loadSpamClassifier = do
-  spam <- bowFromFolder "./.data/train/spam/"
-  ham  <- bowFromFolder "./.data/train/ham/"
-  return $ SpamClassifier spam ham
-
 mkWord :: T.Text -> Word
 mkWord = Word . T.toUpper
 
 normalizeTextToWords :: T.Text -> [Word]
-normalizeTextToWords =
-  map mkWord . T.words . T.map (\c -> if isAlphaNum c then c else ' ')
+normalizeTextToWords = map mkWord . T.words . T.map alphaNumOnly
+  where alphaNumOnly c = if isAlphaNum c then c else ' '
 
 wordToBow :: Word -> Bow
 wordToBow w = Bow $ M.fromList [(w, 1)]
@@ -56,6 +53,26 @@ wordProbability word bow = wordOccurance / numOfWords
   wordOccurance = fromIntegral . fromMaybe 0 . M.lookup word . bowToMap $ bow
   numOfWords    = fromIntegral . wordsCount $ bow
 
+spamProbabilityWord :: Word -> SpamModel -> Maybe Float
+spamProbabilityWord word model | wordSeen word model = Just $ pws / (pws + phs)
+                               | otherwise           = Nothing
+ where
+  pws = wordProbability word (spam model)
+  phs = wordProbability word (ham model)
+
+spamProbabilityText :: T.Text -> SpamModel -> Maybe Float
+spamProbabilityText text model = if null ps
+  then Nothing
+  else Just $ pp / (pp + ipp)
+ where
+  ps  = mapMaybe (`spamProbabilityWord` model) (normalizeTextToWords text)
+  pp  = product ps
+  ipp = product $ map (1.0 -) ps
+
+wordSeen :: Word -> SpamModel -> Bool
+wordSeen word (SpamModel (Bow spamBow) (Bow hamBow)) =
+  M.member word spamBow || M.member word hamBow
+
 bowFromFile :: FilePath -> IO Bow
 bowFromFile path = textToBow <$> T.readFile path
 
@@ -63,21 +80,8 @@ bowFromFolder :: FilePath -> IO Bow
 bowFromFolder folderPath =
   fold <$> (listDirectory folderPath >>= mapM (bowFromFile . (folderPath <>)))
 
-wordSeen :: SpamClassifier -> Word -> Bool
-wordSeen sc w = isJust sm || isJust hm
- where
-  sm = M.lookup w . bowToMap . spam $ sc
-  hm = M.lookup w . bowToMap . ham $ sc
-
-spamProbabilityWord :: SpamClassifier -> Word -> Float
-spamProbabilityWord sc w = if pws + phs == 0.0 then 0.0 else pws / (pws + phs)
- where
-  pws = wordProbability w . spam $ sc
-  phs = wordProbability w . ham $ sc
-
-spamProbabilityText :: SpamClassifier -> T.Text -> Float
-spamProbabilityText sc text = if pp + ipp == 0.0 then 0.0 else pp / (pp + ipp)
- where
-  ps  = map (spamProbabilityWord sc) (normalizeTextToWords text)
-  pp  = product ps
-  ipp = product $ map (1.0 -) ps
+loadSpamModel :: IO SpamModel
+loadSpamModel = do
+  spamBow <- bowFromFolder "./.data/train/spam/"
+  hamBow  <- bowFromFolder "./.data/train/ham/"
+  return $ SpamModel spamBow hamBow
